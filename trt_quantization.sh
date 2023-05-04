f22f0977b1fad41291eb533a0876c81f65a4e77d
MarioGG23 <131919838+MarioGG23@users.noreply.github.com>
2023-05-04 12:35:20 -0400

    Update trt_quantization.sh

--- trt_quantization.sh
+++ trt_quantization.sh
@@ -1,8 +1,8 @@
 #Quantization
 
 #FP32
-/usr/src/tensorrt/bin/trtexec --onnx=/home/rhernandez/practica/casia-best_model.onnx --saveEngine=/home/rhernandez/Desktop/modelos/casia_batch1_fp32.trt
-/usr/src/tensorrt/bin/trtexec --loadEngine=/home/rhernandez/Desktop/modelos/casia_batch1_fp32.trt
+/usr/src/tensorrt/bin/trtexec --onnx=/home/rhernandez/Desktop/practica/casia-best_model.onnx --saveEngine=/home/rhernandez/Desktop/casia_batch1_fp32.trt
+/usr/src/tensorrt/bin/trtexec --loadEngine=/home/rhernandez/Desktop/casia_batch1_fp32.trt
 
 #FP16
 #/usr/src/tensorrt/bin/trtexec --onnx=/home/nano/resnet50_onnx.onnx --saveEngine=/home/nano/trt_models/resnet50_batch1_fp16.trt --fp16
