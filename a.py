b174d37d61dd974a91b9254cd799cc216bb56977
Thadli Guerra <130626268+tatatadlisixti4@users.noreply.github.com>
2023-05-09 13:13:17 -0400

    Add files via upload

--- a.py
+++ a.py
@@ -7,8 +7,8 @@ ubicacion1 = '/home/rhernandez/onnx_models/' + modelo + '.onnx'
 ubicacion1_sinext = ubicacion1.split('.')
 
 #trt = aux2 + 'trt_quantization.txt' #aux 2 es donde se va a guardar el trt
-#ruta = '/Users/thadliguerra/Desktop/trt_sh/'
-ruta = '/home/rhernandez/trt_sh/'
+ruta = '/Users/thadliguerra/Desktop/trt_sh/'
+#ruta = '/home/rhernandez/trt_sh/'
 
 if tipo == 'fp32':
     trt =  ruta + 'trt_quantization_' + tipo + '_' + modelo + '.txt'
@@ -16,7 +16,7 @@ if tipo == 'fp32':
     #ubicacion2 = ubicacion1_sinext[0] + '_batch1_fp32.trt' 
     ubicacion2 = '/home/rhernandez/modelos_trt/' + modelo + '_batch1_fp32.trt'
     linea1 = '/usr/src/tensorrt/bin/trtexec --onnx=' + ubicacion1 + ' --saveEngine=' + ubicacion2
-    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2 
+    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2 + ' > ' + modelo + '_batch1_fp32_resultados.txt'
     file = open(trt, "w")
     file.write(linea1 + os.linesep)
     file.write(linea2)
@@ -31,7 +31,7 @@ elif tipo == 'fp16':
     #ubicacion2 = ubicacion1_sinext[0] + '_batch1_fp16.trt' 
     ubicacion2 = '/home/rhernandez/modelos_trt/' + modelo + '_batch1_fp16.trt'
     linea1 = '/usr/src/tensorrt/bin/trtexec --onnx=' + ubicacion1 + ' --saveEngine=' + ubicacion2
-    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2 
+    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2 + ' > ' + modelo + '_batch1_fp16_resultados.txt'
     file = open(trt, "w")
     file.write(linea1 + os.linesep)
     file.write(linea2)
@@ -46,7 +46,7 @@ else:
     #ubicacion2 = ubicacion1_sinext[0] + '_batch1_int8.trt'
     ubicacion2 = '/home/rhernandez/modelos_trt/' + modelo + '_batch1_int8.trt'
     linea1 = '/usr/src/tensorrt/bin/trtexec --onnx=' + ubicacion1 + ' --saveEngine=' + ubicacion2
-    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2
+    linea2 = '/usr/src/tensorrt/bin/trtexec --loadEngine=' + ubicacion2 + ' > ' + modelo + '_batch1_int8_resultados.txt'
     file = open(trt, "w")
     file.write(linea1 + os.linesep)
     file.write(linea2)
