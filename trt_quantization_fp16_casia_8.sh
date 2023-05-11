fe68a2fb5324c5b4b32d8b0fcefe1f1f827cfddb
Thadli Guerra <130626268+tatatadlisixti4@users.noreply.github.com>
2023-05-10 22:53:55 -0400

    Add files via upload

--- trt_quantization_fp16_casia_8.sh
+++ trt_quantization_fp16_casia_8.sh
@@ -1,2 +1,2 @@
-/usr/src/tensorrt/bin/trtexec --onnx=/home/rhernandez/onnx_models/casia_dynamic_batch.onnx --saveEngine=/home/rhernandez/modelos_trt/casia_batch8_fp16.trt --shapes='input':8x128x128
+/usr/src/tensorrt/bin/trtexec --onnx=/home/rhernandez/onnx_models/casia_dynamic_batch.onnx --saveEngine=/home/rhernandez/modelos_trt/casia_batch8_fp16.trt --shapes='input'\:8x128x128
 /usr/src/tensorrt/bin/trtexec --loadEngine=/home/rhernandez/modelos_trt/casia_batch8_fp16.trt > casia_batch8_fp16.txt
